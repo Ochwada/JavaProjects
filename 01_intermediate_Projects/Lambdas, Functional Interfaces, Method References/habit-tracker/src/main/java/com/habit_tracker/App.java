926c3f63f1ef8eae897cf62774ac3e6daa8e370a
ochwada <nakhulolinda@yahoo.com>
2025-06-12 15:09:29 +0200

    added Habit, HabitProcessors and App class and methods

--- 01_intermediate_Projects/Lambdas, Functional Interfaces, Method References/habit-tracker/src/main/java/com/habit_tracker/App.java
+++ 01_intermediate_Projects/Lambdas, Functional Interfaces, Method References/habit-tracker/src/main/java/com/habit_tracker/App.java
@@ -1,13 +1,46 @@
 package com.habit_tracker;
 
+import java.util.*;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // Create an instance of HabitProcessor to access filtering functionality.
+        HabitProcessor processor = new HabitProcessor();
+
+        /**
+         * Create a list of Habit objects
+         * Each Habit contains
+         * - a name (String)
+         * - a completedToday status (boolean)
+         * - a category (String)
+         * */
+        List<Habit> habits = List.of(
+                new Habit("Drink water", true, "Health"),
+                new Habit("Morning run", false, "Fitness"),
+                new Habit("Meditation", false, "Mindfulness"),
+                new Habit("Sleep 8 hours", true, "Health")
+        );
+
+        /**
+         * Use HabitProcessor's filterHabits() method to filter habits that are not done.
+         * The lambda expression: habit -> !habit.isCompletedToday().
+         * - Reads as: "Keep habits where isCompletedToday() returns false".
+         * The method returns a new list of pending habits
+         * */
+        List<Habit> pendingHabits = processor.filterHabits(
+                habits, habit -> !habit.isCompletedToday()
+        );
+
+        // Option 1: Print all habits using stream
+        processor.processHabits(pendingHabits,
+                habit -> System.out.println("Don't forget: " + habit ));
+
+        // Option 2: Print all habits using stream
+        System.out.println();
+        processor.processHabits(pendingHabits,
+                habit -> System.out.println("Don't forget: [" + habit.getName() + "]"));
     }
 }

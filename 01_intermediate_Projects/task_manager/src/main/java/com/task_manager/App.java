20db3269afd7b4d08797c560cd817895ab3184e2
ochwada <nakhulolinda@yahoo.com>
2025-06-12 10:59:34 +0200

    Demo class setup

--- 01_intermediate_Projects/task_manager/src/main/java/com/task_manager/App.java
+++ 01_intermediate_Projects/task_manager/src/main/java/com/task_manager/App.java
@@ -1,13 +1,44 @@
 package com.task_manager;
 
+import java.util.*;
+
 /**
  * Demo Class
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        // Create an instance of TaskProcessor to access filtering functionality.
+        TaskProcessor processor = new TaskProcessor();
+
+
+        /**
+         * Create a list of Task objects.
+         *
+         * Each Task contains:
+         * - a description (String),
+         * - a completion status (boolean),
+         * - and a priority (int).
+         */
+        List<Task> tasks = List.of(
+                new Task("Buy computer", true, 1),
+                new Task("Buy a bike", false, 2),
+                new Task("Pay for Bafog", false, 3)
+        );
+
+
+        /**
+         * 3️Use TaskProcessor's filterTasks() method to filter tasks that are not completed.
+         * The lambda expression: task -> !task.isCompleted()
+         * - Reads as: "Keep tasks where isCompleted() returns false."
+         * The method returns a new list of pending tasks.
+         */
+
+        List<Task> pendingTasks = processor.filterTasks(
+                tasks,
+                task -> !task.isCompleted());
+//        {
+//            // other logics
+//            return !task.isCompleted());
+//        }
     }
 }

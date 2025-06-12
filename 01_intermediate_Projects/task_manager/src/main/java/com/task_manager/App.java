7c998f1da15e34151b7cbccf378e3edc82f31ae8
ochwada <nakhulolinda@yahoo.com>
2025-06-12 09:38:32 +0200

    Update project

---
+++ 01_intermediate_Projects/task_manager/src/main/java/com/task_manager/App.java
@@ -0,0 +1,13 @@
+package com.task_manager;
+
+/**
+ * Demo Class
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

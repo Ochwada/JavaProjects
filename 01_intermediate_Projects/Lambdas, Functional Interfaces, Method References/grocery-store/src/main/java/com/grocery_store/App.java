9b016db283da75bc410536d62e0fa700a35c316f
ochwada <nakhulolinda@yahoo.com>
2025-06-13 09:39:15 +0200

    grocery store project - initial project setup

---
+++ 01_intermediate_Projects/Lambdas, Functional Interfaces, Method References/grocery-store/src/main/java/com/grocery_store/App.java
@@ -0,0 +1,13 @@
+package com.grocery_store;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ 01_intermediate_Projects/Stream API/grocery-store/src/main/java/com/grocery_store/InventoryService.java
@@ -0,0 +1,17 @@
+package com.grocery_store;
+
+
+/**
+ * *******************************************************
+ * Package: com.grocery_store
+ * File: InventoryService.java
+ * Author: Ochwada
+ * Date: Friday, 13.Jun.2025, 9:38 AM
+ * Description:
+ * Objective:
+ * *******************************************************
+ */
+
+
+public class InventoryService {
+}

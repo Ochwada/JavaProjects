185dca91bfe10c0be88e326fb8fa1a7757dbdb4f
ochwada <nakhulolinda@yahoo.com>
2025-06-16 09:45:13 +0200

    Initial project setup

---
+++ 01_intermediate_Projects/Stream API/book-library-analyzer/src/main/java/com/book_library_analyzer/App.java
@@ -0,0 +1,13 @@
+package com.book_library_analyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

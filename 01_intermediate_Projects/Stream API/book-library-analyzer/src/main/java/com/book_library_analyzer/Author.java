a57777c3cc2a72ef4b818353618eb41960e3536f
ochwada <nakhulolinda@yahoo.com>
2025-06-16 10:17:13 +0200

    Book and author classes added

---
+++ 01_intermediate_Projects/Stream API/book-library-analyzer/src/main/java/com/book_library_analyzer/Author.java
@@ -0,0 +1,42 @@
+package com.book_library_analyzer;
+
+
+import java.util.List;
+
+/**
+ * *******************************************************
+ * Package: com.book_library_analyzer
+ * File: Author.java
+ * Author: Ochwada
+ * Date: Monday, 16.Jun.2025, 9:45 AM
+ * Description:  It represents an author in the Book Library System
+ * Objective: Domain object class (Author)
+ * *******************************************************
+ */
+
+public class Author {
+
+    private final String name;
+    private final List<Book> books;
+
+    /**
+     * Constructs an Author object with the given list of books and author name.
+     *
+     * @param books the list of Book objects written by the author.
+     * @param name  the name of the author.
+     */
+    public Author(List<Book> books, String name) {
+        this.books = books;
+        this.name = name;
+    }
+
+    // Setter
+    public String getName() {
+        return name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+}
---
+++ 01_intermediate_Projects/Stream API/book-library-analyzer/src/main/java/com/book_library_analyzer/Book.java
@@ -0,0 +1,53 @@
+package com.book_library_analyzer;
+
+
+/**
+ * *******************************************************
+ * Package: com.book_library_analyzer
+ * File: Book.java
+ * Author: Ochwada
+ * Date: Monday, 16.Jun.2025, 9:51 AM
+ * Description: It represents a book in the Book Library System
+ * Objective: Domain object class (Book)
+ * *******************************************************
+ */
+
+
+public class Book {
+
+    private final String title;
+    private final String genre;
+    private final double rating;
+
+
+    /**
+     * Constructs a Book object with the specified title, genre, and rating.
+     *
+     * @param title  the title of the book
+     * @param genre  the genre of the book
+     * @param rating the rating of the book (e.g., from 0.0 to 5.0)
+     */
+    public Book(String title, String genre, double rating) {
+        this.title = title;
+        this.genre = genre;
+        this.rating = rating;
+    }
+
+    // Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{title='" + title + "', genre='" + genre + "', rating=" + rating + "}";
+    }
+}
